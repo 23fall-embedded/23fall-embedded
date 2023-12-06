d5a97f2fd688ba3113ce02d0783afde41a255ac3
Dusker233 <1187305740@qq.com>
2023-12-06 18:18:36 +0800

    20231206-1

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from utils.aliyun import aliLink, mqttd, rpi
 import time, json
 import RPi.GPIO as GPIO
 import utils.dht11 as dht11
-
+import utils.led as led
 
 # 三元素（iot后台获取）
 ProductKey = "k0kh4u9Sfng"
@@ -14,6 +14,9 @@ DeviceSecret = "7d716841ca95881559e7a8bf7f1ff06c"
 POST = "/sys/k0kh4u9Sfng/pi-1/thing/event/property/post"  # 上报消息到云
 POST_REPLY = "/sys/k0kh4u9Sfng/pi-1/thing/event/property/post_reply"
 SET = "/sys/k0kh4u9Sfng/pi-1/thing/service/property/set"  # 订阅云端指令
+user_get = "/k0kh4u9Sfng/pi-1/user/get"
+user_update = "/k0kh4u9Sfng/pi-1/user/update"
+user_update_err = "/k0kh4u9Sfng/pi-1/user/update/error"
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -24,11 +27,21 @@ instance = dht11.DHT11(17)
 
 # 消息回调（云端下发消息的回调函数）
 def on_message(client, userdata, msg):
-    # print(msg.payload)
+    print(msg.payload)
+    # Msg = msg.payload.decode('utf-8')
     Msg = json.loads(msg.payload)
-    switch = Msg["params"]["PowerLed"]
-    rpi.powerLed(switch)
-    print(msg.payload)  # 开关值
+    print(Msg)
+    if "led" in Msg:
+        col = Msg["led"]
+        print(col)
+        if col == "off":
+            led.led_off()
+        else:
+            led.led_on(col)
+
+    # switch = Msg["params"]["PowerLed"]
+    # rpi.powerLed(switch)
+    # print(msg.payload)  # 开关值
 
 
 # 连接回调（与阿里云建立链接后的回调函数）
@@ -44,6 +57,9 @@ Server, ClientId, userNmae, Password = aliLink.linkiot(
 # mqtt链接
 mqtt = mqttd.MQTT(Server, ClientId, userNmae, Password)
 mqtt.subscribe(SET)  # 订阅服务器下发消息topic
+mqtt.subscribe(user_get)
+mqtt.subscribe(user_update)
+mqtt.subscribe(user_update_err)
 mqtt.begin(on_message, on_connect)
 
 
@@ -70,3 +86,6 @@ while True:
         print(JsonUpdataMsn)
 
         mqtt.push(POST, JsonUpdataMsn)  # 定时向阿里云IOT推送我们构建好的Alink协议数据
+
+
+# {"id": 982943, "version": "1.0", "params": {"temperature": 26.7, "humidity": 36.0, "cpuTemperature": 57.4}, "method": "thing.event.property.post"}
---
+++ utils/led.py
@@ -0,0 +1,27 @@
+from gpiozero import LED
+import time
+
+ledr = LED(16)
+ledg = LED(21)
+ledy = LED(20)
+
+def led_off():
+    if ledr.is_lit:
+        ledr.off()
+    if ledg.is_lit:
+        ledg.off()
+    if ledy.is_lit:
+        ledy.off()
+
+def led_on(col):
+    led_off()
+    if col == "all":
+        ledr.on()
+        ledg.on()
+        ledy.on()
+    if col == "red":
+        ledr.on()
+    elif col == "green":
+        ledg.on()
+    else:
+        ledy.on()
