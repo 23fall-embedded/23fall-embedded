bbe938b5ddd3335e9ca8ea336cec9657953416f6
Dusker233 <1187305740@qq.com>
2023-12-17 18:21:04 +0800

    20231217-add-weather-on-ssd3306

--- utils/ssd3306.py
+++ utils/ssd3306.py
@@ -7,11 +7,9 @@ import time
 
 serial = i2c(port=1, address=0x3C)
 device = ssd1306(serial)
-size = 8
 
-font_ch = ImageFont.truetype("platech.ttf", size)
-
-def show(temp, humi, licenses):
+def show(temp, humi, licenses, size):
+    font_ch = ImageFont.truetype("platech.ttf", size)
     with canvas(device) as draw:
         draw.rectangle(device.bounding_box, outline="white", fill="black") # 让屏幕周围显示一个框
         draw.text((10, 10), f"温度：{temp}°C", fill="white", font=font_ch) # col, line
@@ -19,5 +17,16 @@ def show(temp, humi, licenses):
         cnt = 2
         for license in licenses:
             draw.text((10, 10 + (cnt * size)), license, fill="white", font=font_ch)
+            cnt += 1
+        time.sleep(2)
+
+def show2(string, split_char, size):
+    font_ch = ImageFont.truetype("platech.ttf", size)
+    with canvas(device) as draw:
+        draw.rectangle(device.bounding_box, outline="white", fill="black") # 让屏幕周围显示一个框
+        cnt = 0
+        strs = string.split(split_char)
+        for s in strs:
+            draw.text((10, 10 + (cnt * size)), s, fill="white", font=font_ch)
             cnt += 1
         time.sleep(2)
