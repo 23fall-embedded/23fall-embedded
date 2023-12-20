f5ec618c86fe5b73513bd4bf8508ad187e628226
Dusker233 <1187305740@qq.com>
2023-12-20 17:17:21 +0800

    20231220-add-pi-cam

--- utils/ssd3306.py
+++ utils/ssd3306.py
@@ -8,11 +8,12 @@ import time
 serial = i2c(port=1, address=0x3C)
 device = ssd1306(serial)
 
+
 def show(temp, humi, licenses, size):
     font_ch = ImageFont.truetype("platech.ttf", size)
     with canvas(device) as draw:
-        draw.rectangle(device.bounding_box, outline="white", fill="black") # 让屏幕周围显示一个框
-        draw.text((10, 10), f"温度：{temp}°C", fill="white", font=font_ch) # col, line
+        draw.rectangle(device.bounding_box, outline="white", fill="black")  # 让屏幕周围显示一个框
+        draw.text((10, 10), f"温度：{temp}°C", fill="white", font=font_ch)  # col, line
         draw.text((10, 10 + size), f"湿度：{humi}%", fill="white", font=font_ch)
         cnt = 2
         for license in licenses:
@@ -20,10 +21,11 @@ def show(temp, humi, licenses, size):
             cnt += 1
         time.sleep(2)
 
+
 def show2(string, split_char, size):
     font_ch = ImageFont.truetype("platech.ttf", size)
     with canvas(device) as draw:
-        draw.rectangle(device.bounding_box, outline="white", fill="black") # 让屏幕周围显示一个框
+        draw.rectangle(device.bounding_box, outline="white", fill="black")  # 让屏幕周围显示一个框
         cnt = 0
         strs = string.split(split_char)
         for s in strs:
--- weather_test.py
+++ weather_test.py
@@ -5,8 +5,8 @@ import requests
 import utils.ssd3306 as ssd3306
 
 HEFENG_KEY = "8aa110183dd6419092782f94dbe34e1c"
-LOCATION = "122.06,37.53"
 PUBLICID = "HE2312171657561523"
+
 
 def encode(params):
     params = sorted(params.items(), key=lambda x: x[0], reverse=False)
@@ -23,35 +23,65 @@ def encode(params):
     string = cur[:-1] + HEFENG_KEY
     return f"{cur}sign={hashlib.md5(string.encode(encoding='UTF-8')).hexdigest()}"
 
-def test():
+
+def get_req(url):
+    response = requests.get(url)
+    return response.json()
+
+
+def test(loc="威海"):
     while True:
-        params = {
-            "location": LOCATION,
-            "t": time.time(),
-            "publicid": PUBLICID
-        }
+        params = {"location": loc, "t": time.time(), "publicid": PUBLICID}
+        url = f"https://geoapi.qweather.com/v2/city/lookup?{encode(params)}"
+        message = get_req(url)
+        if message["code"] == "200":
+            loc = message["location"][0]["id"]
+        else:
+            raise ValueError("获取 api 失败")
+        params = {"location": loc, "t": time.time(), "publicid": PUBLICID}
         code = encode(params)
         url = f"https://devapi.qweather.com/v7/weather/now?{code}"
-        print(url)
-        response = requests.get(url)
-        message = response.json()
+        message = get_req(url)
+
+        print(message)
 
         if message["code"] == "200":
             now = message["now"]
-            ssd3306.show2(f"现在天气状况：{now['text']}，温度：{now['temp']}°C，体感温度：{now['feelsLike']}°C，湿度：{now['humidity']}%，风向风力：{now['windDir']}{now['windScale']}级", "，", 9)
+            ssd3306.show2(
+                f"现在天气状况：{now['text']}，温度：{now['temp']}°C，体感温度：{now['feelsLike']}°C，湿度：{now['humidity']}%，风向风力：{now['windDir']}{now['windScale']}级",
+                "，",
+                9,
+            )
         else:
             raise ValueError("获取 api 失败")
         url = f"https://devapi.qweather.com/v7/weather/3d?{code}"
-        response = requests.get(url)
-        message = response.json()
+        message = get_req(url)
+        print(message["fxLink"])
         if message["code"] == "200":
             tomorrow = message["daily"][1]
             day2 = message["daily"][2]
-            ssd3306.show2(f"明天白天天气状况：{tomorrow['textDay']}，温度：{tomorrow['tempMin']}到{tomorrow['tempMax']}°C，湿度：{tomorrow['humidity']}%，风向风力：{tomorrow['windDirDay']}{tomorrow['windScaleDay']}级", "，", 9)
-            ssd3306.show2(f"明天夜间天气状况：{tomorrow['textNight']}，温度：{tomorrow['tempMin']}到{tomorrow['tempMax']}°C，风向风力：{tomorrow['windDirNight']}{tomorrow['windScaleNight']}级", "，", 9)
-            ssd3306.show2(f"后天白天天气状况：{day2['textDay']}，温度：{day2['tempMin']}到{day2['tempMax']}°C，湿度：{day2['humidity']}%，风向风力：{day2['windDirDay']}{day2['windScaleDay']}级", "，", 9)
-            ssd3306.show2(f"后天夜间天气状况：{day2['textNight']}，温度：{day2['tempMin']}到{day2['tempMax']}°C，风向风力：{day2['windDirNight']}{day2['windScaleNight']}级", "，", 9)
+            ssd3306.show2(
+                f"明天白天天气状况：{tomorrow['textDay']}，温度：{tomorrow['tempMin']}到{tomorrow['tempMax']}°C，湿度：{tomorrow['humidity']}%，风向风力：{tomorrow['windDirDay']}{tomorrow['windScaleDay']}级",
+                "，",
+                9,
+            )
+            ssd3306.show2(
+                f"明天夜间天气状况：{tomorrow['textNight']}，温度：{tomorrow['tempMin']}到{tomorrow['tempMax']}°C，风向风力：{tomorrow['windDirNight']}{tomorrow['windScaleNight']}级",
+                "，",
+                9,
+            )
+            ssd3306.show2(
+                f"后天白天天气状况：{day2['textDay']}，温度：{day2['tempMin']}到{day2['tempMax']}°C，湿度：{day2['humidity']}%，风向风力：{day2['windDirDay']}{day2['windScaleDay']}级",
+                "，",
+                9,
+            )
+            ssd3306.show2(
+                f"后天夜间天气状况：{day2['textNight']}，温度：{day2['tempMin']}到{day2['tempMax']}°C，风向风力：{day2['windDirNight']}{day2['windScaleNight']}级",
+                "，",
+                9,
+            )
         else:
             raise ValueError("获取 api 失败")
+
 
 test()
